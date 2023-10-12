f9a6bb94b733ff42d7449f4f8873328cd7b7fa8d
Sanju <thekingman573@gmail.com>
2023-10-12 19:53:04 +0530

    updated

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = process.env.PORT || '4000';
 import admin  from './routes/admin.js';
 
+app.use('/assets' , express.static('assets'));
+
 app.set('view engine' , 'ejs')
 app.use('/' , admin)
-
 
 // start the server
 app.listen(port, () => console.log(`Server listening on port ${port}`));
